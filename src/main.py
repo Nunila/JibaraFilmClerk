831268a9a54f120513e289178c8f63d362f8662a
Nunila <nunila.davila@upr.edu>
2020-06-05 03:50:23 -0400

    Added git ignore and decided Movie DB API

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# Jíbara Film Clerk
+
+# Jíbara Movie Clerk will send daily emails containing a movie recommendation based on the user´s desired genres.
+# This will work only locally and will be hard-coded to only fit my favorite movie genres.
+# Movie Genres I prefer: Thriller, Horror, Sci-fi
+
+# The movie database API to be used is: The Open Movie Database (http://www.omdbapi.com/)
+
+# Read API key from local file
+# Verificar como trabajan los search en este API para ver si se puede escoger una desde el search o randomize la seleccion
+# del search cada vez que se hace.
+# Ya con la movie, se le manda el email al usuario
