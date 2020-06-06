31e02964d2fc0dc760fdc8ca21e758153b85459d
Nunila <nunila.davila@upr.edu>
2020-06-06 02:14:16 -0400

    Emails are now sent with a movie recommendation. Contains name, poster and release year. Working on prettier email format.

--- src/recommendation_methods.py
+++ src/recommendation_methods.py
@@ -4,6 +4,10 @@ import requests
 import urllib.parse as urlparse
 from random import randint
 
+from string import Template
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 # TMDb returns results as only one page. To select a movie, I will randomize the page # and then select a random entry
 
 
@@ -23,7 +27,7 @@ class MovieRecommendation:
         url_parse = url_parse._replace(query=url_new_query)
         full_url = urlparse.urlunparse(url_parse)
 
-        result_index = randint(0, 20)
+        result_index = randint(0, 19)
         r = requests.get(url=full_url, )
         data = r.json()
         movie_result = data['results'][result_index]
@@ -61,3 +65,50 @@ class MovieRecommendation:
         poster_size = "w342"  # w185
         complete_image_path = base_image_path + poster_size + movie_result['poster_path']
         print("Poster path ", complete_image_path)
+        release_year = movie_result['release_date'][:4]
+
+        port = 587  # For starttls
+        smtp_server = "smtp.gmail.com"
+        sender_email = "jibaramovieclerk@gmail.com"
+        receiver_email = "nunimarga_09@hotmail.com"
+        password = self.get_email_password()
+
+        server = smtplib.SMTP(smtp_server, port)
+        server.ehlo()  # Can be omitted
+        server.starttls()
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "Movie Recommendation of the day"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+
+        path = os.path.abspath("src/email_format.txt")
+        print("email format path", path)
+        message_template = self.read_template(path)
+        email_message = message_template.substitute(POSTER=complete_image_path,
+                                                    MovieTitle=movie_result['title'], YEAR=release_year)
+        part2 = MIMEText(email_message, "html")
+        message.attach(part2)
+
+        print("password", password)
+        print("email message", email_message)
+
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
+    def read_template(self, filename):
+        template_file = open(filename, 'r')
+        template_file_content = template_file.read()
+        return Template(template_file_content)
+
+    def get_email_password(self):
+        path = normpath(join(os.getcwd(), "../CredsInfo/JFCInfo.txt"))
+        f = open(path, "r")
+        line = ""
+        while "password" not in line:
+            line = f.readline()
+        password = line.split(":")[1].strip()
+        f.close()
+
+        return password
--- src/static_data.py
+++ src/static_data.py
@@ -81,3 +81,19 @@ movie_genres = [
       "name": "Western"
     }
   ]
+
+backdrop_sizes = [
+      "w300",
+      "w780",
+      "w1280",
+      "original"
+    ]
+poster_sizes = [
+      "w92",
+      "w154",
+      "w185",
+      "w342",
+      "w500",
+      "w780",
+      "original"
+    ]
